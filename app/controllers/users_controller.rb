8f9c8000b071ef5dde3f4b452f9ec6bdd4460813
HeyStenson <h.n.stenson@gmail.com>
2015-09-29 14:38:28 -0700

    fixed all instances of cities_id in the user edit and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
     @posts = @user.posts
-    @city = City.find_by_id(@user.cities_id)
+    @city = City.find_by_id(@user.city_id)
   end
 
   def destroy
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :cities_id)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :city_id)
   end
 
 end
