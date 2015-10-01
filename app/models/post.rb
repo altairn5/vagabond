b4960e15c91937a7eb3b562891da9bf933181d12
HeyStenson <h.n.stenson@gmail.com>
2015-09-30 18:52:46 -0700

    switching branches

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,27 @@
 class Post < ActiveRecord::Base
 
+
+
 	belongs_to :user
 	belongs_to :city
 
 	validates :title, length: { maximum: 200 }
 
 end
+
+# attr_accessor :keywords
+#   @@alchemy_url ||= ENV["ALCHEMY_URL"]
+
+#   def get_keywords
+#     res = Typhoeus.get(
+#       @@alchemy_url,
+#       params: {
+#         apikey: ENV["ALCHEMY_APIKEY"],
+#         text: content,
+#         maxRetrieve: 10,
+#         outputMode: "json"
+#       }
+#     )
+#     words = JSON.parse(res.body)["keywords"].map { |w| w['text'] }
+#     @keywords = words.join(" | ")
+#   end
