fa0794b53847b14c343c29ed38048b42be4ab42c
HeyStenson <h.n.stenson@gmail.com>
2015-09-30 21:09:35 -0700

    saving before switching branches

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,26 @@
 class Post < ActiveRecord::Base
 
+	attr_accessor :keywords
+  	@@alchemy_url ||= "http://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords"
 
+	  def get_keywords
+	    res = Typhoeus.get(
+	      @@alchemy_url,
+	      params: {
+	        apikey: "d3f607d969d5a875bb1e2e20528b67328b670b0b",
+	        text: content,
+	        maxRetrieve: 10,
+	        outputMode: "json"
+	      }
+	    )
+	    puts "keyword search: " + res.body.to_s
+	   	kwords = JSON.parse(res.body)
+	   	puts  "drilling down:" + kwords['keywords'].to_s
+	   	if kwords['keywords'] != nil
+	        words = kwords['keywords'].map { |w| w['text'] }
+	        @keywords = words.join(" | ")
+	    end
+	  end
 
 	belongs_to :user
 	belongs_to :city
@@ -9,19 +29,3 @@ class Post < ActiveRecord::Base
 
 end
 
-# attr_accessor :keywords
-#   @@alchemy_url ||= ENV["ALCHEMY_URL"]
-
-#   def get_keywords
-#     res = Typhoeus.get(
-#       @@alchemy_url,
-#       params: {
-#         apikey: ENV["ALCHEMY_APIKEY"],
-#         text: content,
-#         maxRetrieve: 10,
-#         outputMode: "json"
-#       }
-#     )
-#     words = JSON.parse(res.body)["keywords"].map { |w| w['text'] }
-#     @keywords = words.join(" | ")
-#   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929175554) do
+ActiveRecord::Schema.define(version: 20151001020026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20150929175554) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
@@ -36,6 +44,12 @@ ActiveRecord::Schema.define(version: 20150929175554) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "city_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
